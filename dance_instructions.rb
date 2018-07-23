3fd9c23ec5009b7a4ac9ed028e20821cefbd3ac0
Jennifer Okwara <jokwara03@gmail.com>
2018-07-23 20:14:55 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,10 +1,10 @@
 
 # Code your methods below
 def the_dance1
-puts left foot
-puts right foot
-puts raise right hand
-puts raise right hand
+puts "left foot"
+puts "right foot"
+puts "raise right hand"
+puts "raise right hand"
 end
 the_dance1
 
